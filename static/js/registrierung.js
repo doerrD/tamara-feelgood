46f00d00eddb9e006ee709b02e30c16dd985c4f9
Dominik Dörr <dominik.doerr@softaware.at>
2023-10-10 22:53:31 +0200

    add honypot handling

--- static/js/registrierung.js
+++ static/js/registrierung.js
@@ -4,14 +4,25 @@ $(function(){ // .ready() callback, is only executed when the DOM is fully loade
       
         const myForm = event.target;
         const formData = new FormData(myForm);
-        
-        fetch("/", {
-          method: "POST",
-          headers: { "Content-Type": "application/x-www-form-urlencoded" },
-          body: new URLSearchParams(formData).toString(),
-        })
-          .then(() => document.getElementById("success").className = "show-result")
-          .catch((error) => alert(error));
+
+        var exitSubmit = false;
+        for(var pair of formData.entries()){
+            if (pair[0] === 'goodinput' && !(pair[1] === '' || pair[1] == null || pair[1] === undefined)) {
+                exitSubmit = true;
+            }
+        }
+
+        if (!exitSubmit) {
+            fetch("/", {
+                method: "POST",
+                headers: { "Content-Type": "application/x-www-form-urlencoded" },
+                body: new URLSearchParams(formData).toString(),
+              })
+                .then(() => document.getElementById("success").className = "show-result")
+                .catch((error) => alert(error));
+        } else {
+            console.log('bot-alert');
+        }
       };
       
       document
